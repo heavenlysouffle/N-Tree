d30164cf01aa48b1189574952ec939eed97d89ee
Polina Bykova <polina.bykova.pb@gmail.com>
2024-01-02 17:47:11 +0100

    Main Activity swipe to left action

--- app/src/main/java/com/example/n_tree/MainActivity.kt
+++ app/src/main/java/com/example/n_tree/MainActivity.kt
@@ -2,26 +2,89 @@ package com.example.n_tree
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import kotlin.math.abs
 
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), GestureDetector.OnGestureListener {
+
+    lateinit var gestureDetector: GestureDetector
+    var x1:Float = 0.0f
+    var x2:Float = 0.0f
+    var y1:Float = 0.0f
+    var y2:Float = 0.0f
+
+    companion object {
+        const val MIN_DISTANCE = 150
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         title = "N-Tree"
+        gestureDetector = GestureDetector(this, this)
+    }
 
-        val button_login:Button = findViewById(R.id.main_to_login)
-        button_login.setOnClickListener {
-            val intent = Intent(this@MainActivity, LoginActivity::class.java)
-            startActivity(intent)
-        }
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        gestureDetector.onTouchEvent(event)
+        when (event?.action) {
+            // when we start to swipe
+            0 -> {
+                x1 = event.x
+                y1 = event.y
+            }
+            // when we end the swipe
+            1 -> {
+                    x2 = event.x
+                    y2 = event.y
 
-        val button_register:Button = findViewById(R.id.main_to_register)
-        button_register.setOnClickListener {
-            val intent = Intent(this@MainActivity, RegisterActivity::class.java)
-            startActivity(intent)
-        }
+                    val valueX:Float = x2-x1
+
+                    if (abs(valueX) > MIN_DISTANCE) {
+                        if (x2 < x1) {
+                            Toast. makeText(this, "yippe!", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+                            startActivity(intent)
+                        }
+                    }
+                }
+            }
+
+        return super.onTouchEvent(event)
+    }
+
+    override fun onDown(e: MotionEvent): Boolean {
+        return false
+    }
+
+    override fun onShowPress(e: MotionEvent) {
+    }
+
+    override fun onSingleTapUp(e: MotionEvent): Boolean {
+        return false
+    }
+
+    override fun onScroll(
+        e1: MotionEvent?,
+        e2: MotionEvent,
+        distanceX: Float,
+        distanceY: Float
+    ): Boolean {
+        return false
+    }
+
+    override fun onLongPress(e: MotionEvent) {
+    }
+
+    override fun onFling(
+        e1: MotionEvent?,
+        e2: MotionEvent,
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
+        return false
     }
 }
