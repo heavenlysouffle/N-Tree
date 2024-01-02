bd2a9a0e82430d5833c921e6d01ffe078dafe058
Taras Faidiuk <taras.fid@gmail.com>
2024-01-02 16:21:54 +0200

    feature: feature: add init LoginActivity BL

--- app/src/main/java/com/example/n_tree/LoginActivity.kt
+++ app/src/main/java/com/example/n_tree/LoginActivity.kt
@@ -1,9 +1,18 @@
 package com.example.n_tree
 
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.IOException
 
 class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,10 +20,47 @@ class LoginActivity : ComponentActivity() {
         setContentView(R.layout.activity_login)
         title = "N-Tree"
 
-        val button_register: Button = findViewById(R.id.login_to_register)
-        button_register.setOnClickListener {
-            val i = Intent(this@LoginActivity, RegisterActivity::class.java)
-            startActivity(i)
+        val textFieldEmail = findViewById<EditText>(R.id.text_field_email)
+        val textFieldPassword = findViewById<EditText>(R.id.text_field_password)
+
+        val buttonRegister: Button = findViewById(R.id.login_btn)
+        buttonRegister.setOnClickListener {
+            val URL = "http://185.69.154.93/api/auth/login"
+            if (URL.isNotEmpty()) {
+                val fetchData = OkHttpClient()
+                val formBody = FormBody.Builder()
+                    .add("email", textFieldEmail.text.toString())
+                    .add("password", textFieldPassword.text.toString())
+                    .build()
+
+                val request = Request.Builder()
+                    .url(URL)
+                    .post(formBody)
+                    .build()
+
+                fetchData.newCall(request).enqueue(object : Callback {
+                    override fun onFailure(call: Call, e: IOException) {
+                        e.printStackTrace()
+                    }
+
+                    override fun onResponse(call: Call, response: Response) {
+                        response.use {
+                            if (!response.isSuccessful) {
+                                Log.e("TAG", "Request failed with status code: ${response.code}")
+                                Log.e("TAG", "Response body: ${response.body?.string()}")
+                            } else {
+                                val body = response.body?.string()
+                                val jsonObject = JSONObject(body.toString())
+                                val token = jsonObject.getString("token")
+
+                                Log.i("TAG", token)
+                            }
+                        }
+                    }
+                })
+            } else {
+                println("Url was empty")
+            }
         }
     }
 }
